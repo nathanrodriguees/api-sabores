4f1a85b6745aaf68c05ce8955e4d357e76e4d0ad
yThanNr7 <149441161+yThanNr7@users.noreply.github.com>
2024-03-28 22:00:44 -0300

    Creating API for flavors

---
+++ API_28_03/src/main/java/br/edu/senaisp/controller/SaborController.java
@@ -0,0 +1,85 @@
+package br.edu.senaisp.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import br.edu.senaisp.dao.SaborDAO;
+import br.edu.senaisp.model.Sabor;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/sabores")
+public class SaborController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		SaborDAO dao = new SaborDAO();
+
+		List<Sabor> sabores = dao.lista();
+
+		String json = "";
+		try {
+			Gson gson = new Gson();
+			json = gson.toJson(sabores);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		resp.getWriter().append(json);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		BufferedReader br = req.getReader();
+		String json = "";
+		String linha = "";
+
+		while ((linha = br.readLine()) != null) {
+			json += linha;
+		}
+
+		Gson gson = new Gson();
+		Sabor sabor = gson.fromJson(json, Sabor.class);
+		SaborDAO dao = new SaborDAO();
+		dao.altera(sabor);
+		resp.getWriter().append("Funfou");
+
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String tmp = req.getParameter("id");
+		int id = Integer.parseInt(tmp);
+
+		BufferedReader br = req.getReader();
+		String json = "";
+		String linha = "";
+
+		while ((linha = br.readLine()) != null) {
+			json += linha;
+		}
+
+		Gson gson = new Gson();
+		Sabor sabor = gson.fromJson(json, Sabor.class);
+
+		sabor.setId(id);
+		SaborDAO dao = new SaborDAO();
+		dao.altera(sabor);
+		resp.getWriter().append("Funfou");
+
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().append("DELETE");
+	}
+
+}
